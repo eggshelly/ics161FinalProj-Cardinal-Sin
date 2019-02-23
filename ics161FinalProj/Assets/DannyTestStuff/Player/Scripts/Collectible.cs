e3f80f84eda3bbfc48a4217b22ad6c03168ebd4b
jcvong <39819708+jcvong@users.noreply.github.com>
2019-02-23 15:55:03 -0800

    Basic collectible checking per level.

---
+++ ics161FinalProj/Assets/DannyTestStuff/Player/Scripts/Collectible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    void Start()
+    {
+        LevelManager.instance.itemsLeft += 1;
+    }
+    // Start is called before the first frame update
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        LevelManager.instance.itemsCollected += 1;
+        Destroy(this.gameObject);
+    }
+}
---
+++ ics161FinalProj/Assets/DannyTestStuff/Player/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    private bool exitUnlocked = false;
+    private bool levelOver = false;
+
+    public int itemsLeft = 0;
+    public int itemsCollected = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    void Update()
+    {
+        checkExit();
+    }
+
+    void checkExit()
+    {
+        if (itemsCollected > 0 && (itemsCollected == itemsLeft))
+            exitUnlocked = true;
+    }
+}
