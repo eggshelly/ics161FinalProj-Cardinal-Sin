b2ebbb101f43c4b17195a47c3f3c36fc1f468897
jjlebow@gmail.com <jlebow@uci.edu>
2019-02-23 15:42:34 -0800

    added dialogue scripts

---
+++ ics161FinalProj/Assets/Scripts - World Map/Dialogue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour
+{
+    //public string key;
+    public string named;
+   // [TextArea(3,10)]
+    //public string[] sentences;
+    public string spriteType;
+    public string text;
+
+    public Dialogue(string named, string spriteType, string text)
+    {
+        this.named = named;
+        this.spriteType = spriteType;
+        this.text = text;
+    }
+}
---
+++ ics161FinalProj/Assets/Scripts - World Map/DialogueImport.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class DialogueImport : MonoBehaviour
+{
+    public TextAsset csvFile;
+    private char lineSeparator = '\n';
+    private char fieldSeparator = ',';
+    private Dictionary<int, Dialogue> GameDialogue;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameDialogue = new Dictionary<int, Dialogue>();
+    }
+    void Start()
+    {
+        readData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void readData()
+    {
+        string name = "";
+        string text = "";
+        string sprite = "";
+        int i = 0;
+        string[] parsedFields = new string[0];
+        string[] parsedLines = csvFile.text.Split(lineSeparator);
+
+        foreach(string line in parsedLines)
+        {
+            parsedFields = line.Split(fieldSeparator);
+        } 
+        foreach(string field in parsedFields)
+        {
+            if(i%3 == 0)
+            {
+                name = parsedFields[i];
+            }
+            else if(i%3 == 1)
+            {
+                text = parsedFields[i];
+            }
+            else if(i%3 == 2)
+            {
+                sprite = parsedFields[i];
+                GameDialogue.Add((i-2), new Dialogue(name, text, sprite));
+            }
+            ++i;
+        }
+    }
+}
