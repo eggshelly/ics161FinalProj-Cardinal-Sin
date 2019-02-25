6fff4e4c743de9b76389412d0a6bf8933f1a3cf5
Danny QI <drqi@uci.edu>
2019-02-24 19:34:54 -0800

    Added Level UI

--- ics161FinalProj/Assets/DannyTestStuff/Player/Scripts/LevelManager.cs
+++ ics161FinalProj/Assets/DannyTestStuff/Player/Scripts/LevelManager.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] GameObject collectiblesPanel;
+    [SerializeField] Sprite keySpriteFilled;
+    [SerializeField] Sprite keySpriteEmpty;
+    [SerializeField] float distanceBetween;
+
     //public static LevelManager instance;
     GameObject[] collectibles;
+    Image[] collectiblesSprites;
     public bool[] collected;
 
     public UnityEvent FinishedLevel;
@@ -24,6 +31,7 @@ public class LevelManager : MonoBehaviour
     private void Start()
     {
         collectibles = GameObject.FindGameObjectsWithTag("Collectible");
+        collectiblesSprites = new Image[collectibles.Length];
         collected = new bool[collectibles.Length];
         totalCollectibles = collectibles.Length;
         for(int i = 0; i < collectibles.Length; ++i)
@@ -32,6 +40,7 @@ public class LevelManager : MonoBehaviour
             collComponent.setIndex(i);
             collComponent.Collected.AddListener(CollectedListener);
         }
+        CreateCollectiblesPanel();
     }
 
     void Update()
@@ -50,6 +59,7 @@ public class LevelManager : MonoBehaviour
     void CollectedListener(int index)
     {
         collected[index] = true;
+        UpdateCollectiblesPanel(index);
         itemsCollected += 1;
         checkExit();
     }
@@ -58,5 +68,30 @@ public class LevelManager : MonoBehaviour
     {
         SaveFileManager.instance.AddCollectablesCountToCurrentState(collected);
         SceneManager.LoadScene("TestMap");
+    }
+
+
+    void CreateCollectiblesPanel()
+    {
+        RectTransform r = collectiblesPanel.GetComponent<RectTransform>();
+        Vector3 leftBorder = new Vector3(r.rect.xMin, r.transform.position.y);
+        Debug.Log(distanceBetween);
+        for (int i = 0; i < totalCollectibles; i++)
+        {
+            GameObject newImage = new GameObject();
+            newImage.transform.SetParent(collectiblesPanel.transform, false);
+            newImage.transform.localScale = newImage.transform.localScale * 0.7f;
+            RectTransform rect = newImage.AddComponent<RectTransform>();
+            rect.anchoredPosition = new Vector2(leftBorder.x + distanceBetween/2 + distanceBetween * i, newImage.transform.localPosition.y);
+
+            Image sr = newImage.AddComponent<Image>();
+            sr.sprite = keySpriteEmpty;
+            collectiblesSprites[i] = sr;
+        }
+    }
+
+    void UpdateCollectiblesPanel(int index)
+    {
+        collectiblesSprites[index].sprite = keySpriteFilled;
     }
 }
