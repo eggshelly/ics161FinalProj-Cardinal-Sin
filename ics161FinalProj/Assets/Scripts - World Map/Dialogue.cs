8161c7e464fa6f06819a328be511aef1272ed5e9
jjlebow@gmail.com <jlebow@uci.edu>
2019-03-04 21:41:57 -0800

    added backgorunds to dialogue objects

--- ics161FinalProj/Assets/Scripts - World Map/Dialogue.cs
+++ ics161FinalProj/Assets/Scripts - World Map/Dialogue.cs
@@ -10,12 +10,14 @@ public class Dialogue
     public string text;
     public string sprite;
     public string audio;
+    public string dialogue;
 
-    public Dialogue(string speaker, string text, string sprite, string audio)
+    public Dialogue(string speaker, string text, string sprite, string audio, string dialogue)
     {
         this.speaker = speaker;
         this.text = text;
         this.sprite = sprite;
         this.audio = audio;
+        this.background = background;
     }
 }
